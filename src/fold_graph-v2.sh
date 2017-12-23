cf03ca3c9bb56f7e740da6603c36d8f67350fcbf
r03ert0 <rto@braincatalogue.org>
2017-12-23 22:26:41 +0100

    rearrange files, fix bugs in foldgraph-v2

--- src/fold_graph-v2.sh
+++ src/fold_graph-v2.sh
@@ -6,33 +6,49 @@
 # Output: A fold graph plus a series of intermediate results
 
 mesh_file="$1"
+
 mesh=${mesh_file%.ply}
-echo $mesh $mesh_file
+echo mesh name: $mesh
+echo mesh file: $mesh_file
+
+# file references
 holes_surf=${mesh}_holesSurf.ply
 holes_vol=${mesh}_holesVol.off
-scrpt=${mesh}_script.mlx
 skeleton=${mesh}_skel.cgal
 skeleton_correspondances=${mesh}_corresp.cgal
 skeleton_curves=${mesh}_skel_curves.txt
 skeleton_graph=${mesh}_skel_graph.txt
 
-mg=/Users/roberto/Applications/brainbits/m.meshgeometry/meshgeometry_mac
-v=/Users/roberto/Applications/brainbits/v.volume/volume
-ms=/Users/roberto/Applications/brainbits/v.mesher/mesh_a_3d_gray_image
-sk=/Library/WebServer/Documents/tsne-skeleton/skeleton/skeleton/simple_mcfskel_example
-c2c=/Library/WebServer/Documents/tsne-skeleton/skeleton/cgal2curves.py
-c2g=/Library/WebServer/Documents/tsne-skeleton/skeleton/cgal2graph.py
+# binary references
+mg=../bin/meshgeometry/meshgeometry_mac
+v=../bin/volume/volume
+ms=../bin/mesher/mesh_a_3d_gray_image
+sk=../bin/skeleton/skeleton/skeleton
+c2c=../bin/skeleton/cgal2curves.py
+c2g=../bin/skeleton/cgal2graph.py
+
+# processing
 
 echo "1. Light Laplace smooth, compute mean curvature, remove sulci, light Laplace smooth"
 $mg -i $mesh_file -laplaceSmooth 0.5 10 -curv -addVal -0.1 -level 0 -removeVerts -laplaceSmooth 0.5 10 -o $holes_surf
 
-echo "2. Extrude the mesh, Laplace smooth one more time"
-read mx my mz sx sy sz <<<$($mg -i $holes_surf -size -printCentre|awk '/size/{split($2,s,",")}/centre/{split($2,c,",")}END{s[1]+=2;s[2]+=2;s[3]+=2;print c[1]-s[1]/2,c[2]-s[2]/2,c[3]-s[3]/2,s[1],s[2],s[3]}')
-read dx dy dz <<<$(echo 0.25 0.25 0.25)
+$mg -i $holes_surf -boundingBox
 
-$v -new $sx,$sy,$sz,$dx,$dy,$dz,$mx,$my,$mz -strokeMesh $orig -not -connected 0,0,0,0 -o $tmp.nii.gz
-$v -new $sx,$sy,$sz,$dx,$dy,$dz,$mx,$my,$mz -strokeMesh $gyri -dilate 4 -and $tmp.nii.gz -mult 10000 -boxFilter 1 2 -o $tmp.inr
-$ms $tmp.inr $holes_vol
+echo "2. Extrude the mesh, Laplace smooth one more time"
+read offx offy offz dimx dimy dimz <<<$($mg -i $holes_surf -size -printCentre|awk '/size/{split($2,s,",")}/centre/{split($2,c,",")}END{s[1]+=10;s[2]+=10;s[3]+=10;print c[1]-s[1]/2,c[2]-s[2]/2,c[3]-s[3]/2,int(s[1]+0.5),int(s[2]+0.5),int(s[3]+0.5)}')
+read pixdimx pixdimy pixdimz <<<$(echo 0.25 0.25 0.25)
+dimx=$(echo $dimx/$pixdimx|bc)
+dimy=$(echo $dimy/$pixdimy|bc)
+dimz=$(echo $dimz/$pixdimz|bc)
+echo volume dimensions: $dimx, $dimy, $dimz
+echo pixel dimensions: $pixdimx, $pixdimy, $pixdimz
+echo offsets: $offx, $offy, $offz
+
+$v -new $dimx,$dimy,$dimz,$pixdimx,$pixdimy,$pixdimz,$offx,$offy,$offz -strokeMesh $mesh_file -not -connected 0,0,0,0 -o tmp.nii.gz
+
+$v -new $dimx,$dimy,$dimz,$pixdimx,$pixdimy,$pixdimz,$offx,$offy,$offz -strokeMesh $holes_surf -dilate 4 -and tmp.nii.gz -mult 10000 -boxFilter 1 2 -o tmp.inr -o holes.nii.gz
+
+$ms tmp.inr $holes_vol
 
 echo "3. Skeletonise"
 $sk $holes_vol $skeleton $skeleton_correspondances
--- src/fold_graph_all.sh
+++ src/fold_graph_all.sh
@@ -1,8 +1,6 @@
-dir=/Users/roberto/Documents/2016_06BrainCatalogue\;Osoianu/BrainCatalogueWorkflow/meshes_centered
+f=". fold_graph-v2.sh"
 
-f="source /Users/roberto/Desktop/fold_graph/fold_graph.sh"
-
-cat subjects.txt| while read s; do
+cat ../data/raw-data/subjects.txt| while read s; do
     echo $s;
     base=${s%/*}
     sub=${s#*/}
