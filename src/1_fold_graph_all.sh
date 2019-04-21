589b8389015a80efd2aebe7421bc5f1b8d8e0b85
r03ert0 <rto@braincatalogue.org>
2019-04-21 11:59:49 +0200

    add holes-vol entry point to fold_graph-v4

--- src/1_fold_graph_all.sh
+++ src/1_fold_graph_all.sh
@@ -6,26 +6,33 @@ DEST="../data/derived/skeleton"
 # external command
 f="./fold_graph-v4.sh"
 
-cat ../data/baboon.txt| while read s; do
-    echo s: $s
+cat ../data/douroucouli.txt| while read -r s; do
+    echo "s: $s"
     mesh=${s#*/}
     sub=${s%/*}
 
     mkdir -p "$DEST/$sub";
     args="-i $RAW/$s -o $DEST/$sub"
 
-    holes="$RAW/$sub/${mesh%.ply}_holesSurf.ply"
-    echo "Looking for $holes"
-    if [ -f $holes ]; then
-        echo "Using precomputed mesh without sulci"
-        args="${args} -p $holes"
+    holes_vol="$RAW/$sub/${mesh%.ply}_holesVol.ply"
+    echo "Looking for $holes_vol volume"
+    if [ -f "$holes_vol" ]; then
+        echo "Using precomputed volumetric mesh without sulci"
+        args="${args} -q $holes_vol"
+    else
+        holes_surf="$RAW/$sub/${mesh%.ply}_holesSurf.ply"
+        echo "Looking for $holes_surf surface"
+        if [ -f "$holes_surf" ]; then
+            echo "Using precomputed surface mesh without sulci"
+            args="${args} -p $holes_surf"
+        fi
     fi
 
     config="$RAW/$sub/config.txt"
     echo "Looking for $config"
-    if [ -f $config ]; then
+    if [ -f "$config" ]; then
         echo "Using configuration file"
-        x=$(sed 's/^/--/' $config)
+        x=$(sed 's/^/--/' "$config")
         args="$args $x"
     fi
 
