0c335c3d8d10a7e03d5ed024f4ca4c913f9dd9fd
r03ert0 <rto@braincatalogue.org>
2019-12-16 10:38:16 +0100

    foldgraph v5, uses meshgeometry for extruding

--- src/fold_graph.sh
+++ src/fold_graph.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# fold_graph v4
-# Roberto Toro, April 2017
+# fold_graph v5
+# Roberto Toro, December 2019
 # Usage:
 #    source fold_graph mesh.ply destination_dir [mesh_with_holes.ply]
 # Input: A mesh in ply format
@@ -11,26 +11,13 @@
 # directory of the current script
 MY_DIR="`dirname \"$0\"`"
 
-# defaults
-level=1000
-decimation=20
-vox_dim=0.25
-
 # parse arguments
 while [[ $# -gt 0 ]]
 do
 key="$1"
 case $key in
-    -d|--decimation-ratio)
-    decimation="$2"
-    shift; shift
-    ;;
     -i|--input)
     src_mesh_file="$2"
-    shift; shift
-    ;;
-    -l|--isosurface-level)
-    level="$2"
     shift; shift
     ;;
     -o|--output-dir)
@@ -43,10 +30,6 @@ case $key in
     ;;
     -q|--precomputed-holes-vol)
     precomputed_holes_vol="$2"
-    shift; shift
-    ;;
-    -v|--vox-dim)
-    vox_dim="$2"
     shift; shift
     ;;
     *)
@@ -67,7 +50,6 @@ echo "destination: $dst_dir"
 sulc_level0=${dst_dir}${mesh}_sulcLevel0.ply
 sulc_map=${dst_dir}${mesh}_sulcMap.txt
 holes_surf=${dst_dir}${mesh}_holesSurf.ply
-holes_surf_vox=${dst_dir}${mesh}_holesSurfVox.ply
 holes_vol=${dst_dir}${mesh}_holesVol
 skeleton=${dst_dir}${mesh}_skel.cgal
 skeleton_correspondances=${dst_dir}${mesh}_corresp.cgal
@@ -77,8 +59,6 @@ spherical=${dst_dir}${mesh}_spherical.ply
 
 # binary references
 mg="$MY_DIR/../bin/meshgeometry/meshgeometry_mac"
-v="$MY_DIR/../bin/volume/volume"
-mc="$MY_DIR/../bin/marching_cubes/marching_cubes"
 sk="$MY_DIR/../bin/skeleton/skeleton/skeleton"
 c2c="$MY_DIR/../bin/skeleton/cgal2curves.py"
 c2g="$MY_DIR/../bin/skeleton/cgal2graph.py"
@@ -99,36 +79,7 @@ if [ "$precomputed_holes_vol" == "" ]; then
     fi
 
     echo "3. Extrude the mesh"
-    read -r offx offy offz dimx dimy dimz <<<$($mg -i "$holes_surf" -size -printCentre\
-    |awk '/size/{split($2,s,",")}/centre/{split($2,c,",")}END{s[1]+=10;s[2]+=10;s[3]+=10;print c[1]-s[1]/2,c[2]-s[2]/2,c[3]-s[3]/2,int(s[1]+0.5),int(s[2]+0.5),int(s[3]+0.5)}')
-    dimx=$(echo "$dimx/$vox_dim"|bc)
-    dimy=$(echo "$dimy/$vox_dim"|bc)
-    dimz=$(echo "$dimz/$vox_dim"|bc)
-    echo "volume dimensions: $dimx, $dimy, $dimz"
-    echo "voxel dimension: $vox_dim"
-    echo "offsets: $offx, $offy, $offz"
-    echo "Convert mesh coordinates to voxel indices"
-
-    $mg \
-        -i "$holes_surf" \
-        -translate $(awk -v x="$offx" -v y="$offy" -v z="$offz" 'BEGIN{printf"%g %g %g",-x,-y,-z}') \
-        -scale $(awk -v d="$vox_dim" 'BEGIN{e=1/d;printf"%g,%g,%g",e,e,e}') \
-        -o "$holes_surf_vox"
-
-    $v \
-        -new "$dimx,$dimy,$dimz,$vox_dim,$vox_dim,$vox_dim,$offx,$offy,$offz" \
-        -strokeMesh "$holes_surf_vox" \
-        -mult 10000 \
-        -boxFilter 1 1 \
-        -o "${dst_dir}tmp.bin" \
-        -o "${dst_dir}holes.nii.gz"
-
-    $mc "${dst_dir}tmp.bin" "${dst_dir}tmp.hdr.txt" "$level" "$decimation" "$holes_vol.ply"
-
-    # clean up
-    #rm ${dst_dir}tmp.nii.gz
-    rm "${dst_dir}tmp.bin"
-    rm "${dst_dir}tmp.hdr.txt"
+    $mg -i "$holes_surf" -extrude -1 -o "$holes_vol.ply"
 else
     echo "1. Using precomputed volumetric surface without sulci"
     cp "$precomputed_holes_vol" "$holes_vol.ply"
@@ -139,5 +90,5 @@ $mg -i "$holes_vol.ply" -o "$holes_vol.off"
 $sk "$holes_vol.off" "$skeleton" "$skeleton_correspondances"
 
 echo "5. Convert skeleton format, simplify the skeleton into a graph"
-python $c2c "$skeleton" > "$skeleton_curves"
-python $c2g "$skeleton" > "$skeleton_graph"
+python "$c2c" "$skeleton" > "$skeleton_curves"
+python "$c2g" "$skeleton" > "$skeleton_graph"
